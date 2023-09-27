796deec49c66e9919a2178a99d4f4fac4854447b
Hafizh Pratama Budiman <hafizh1086@gmail.com>
2023-09-27 16:29:55 +0800

    v1.5

--- Praktikum 0/src/PRAK001_2210817310007_HafizhPratamaBudiman.java
+++ Praktikum 0/src/PRAK001_2210817310007_HafizhPratamaBudiman.java
@@ -1,6 +1,6 @@
 public class PRAK001_2210817310007_HafizhPratamaBudiman {
     public static void main(String[] args){
-        System.out.println("Selamat Pagi, Hafizh Pratama Budiman");
+        System.out.println("Selamat Morning, Hafizh Pratama Budiman");
         System.out.println("Selamat Siang, Hafizh Pratama Budiman");
         System.out.print("Selamat Malam, Hafizh Pratama Budiman");
     }
