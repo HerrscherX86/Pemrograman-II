19281d7127b584dd239a571af5d693d069a4940b
Hafizh Pratama Budiman <hafizh1086@gmail.com>
2023-10-17 23:10:54 +0800

    v1

---
+++ Praktikum 2/PRAK203-2210817310007-HafizhPratamaBudiman/src/Pegawai.java
@@ -0,0 +1,18 @@
+public class Pegawai {
+    public String nama;
+    public char asal;
+    public String jabatan;
+    public int umur;
+
+    public String getNama() {
+        return nama;
+    }
+
+    public String getAsal() {
+        return asal;
+    }
+
+    public void setJabatan() {
+        this.jabatan = j;
+    }
+}
