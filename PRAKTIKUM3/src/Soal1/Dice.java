834dde551205c4d0349bac8f7fb837e66529cf4f
Hafizh Pratama Budiman <hafizh1086@gmail.com>
2023-10-31 17:36:01 +0800

    v1

---
+++ PRAKTIKUM3/src/Soal1/Dice.java
@@ -0,0 +1,12 @@
+package Soal1;
+import java.util.Random;
+public class Dice {
+    private int Value;
+    public void shuffleValue() {
+        Random rdmVal = new Random();
+        Value = rdmVal.nextInt(6)+1;
+    }
+    public int getValue() {
+        return Value;
+    }
+}
