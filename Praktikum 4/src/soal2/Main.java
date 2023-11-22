02e79cecdb0d3202b63d14cba252f9d7cb897e38
Hafizh Pratama Budiman <hafizh1086@gmail.com>
2023-11-22 08:26:42 +0800

    v1.1

--- Praktikum 4/src/soal2/Main.java
+++ Praktikum 4/src/soal2/Main.java
@@ -2,33 +2,33 @@ package soal2;
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        Scanner Input = new Scanner(System.in);
+        Scanner myInput = new Scanner(System.in);
         System.out.println("Pilih jenis hewan yang ingin diinputkan :");
         System.out.println("1 = Kucing");
         System.out.println("2 = Anjing");
         System.out.print("Masukkan pilihan : ");
-        int pilihan = Input.nextInt();
+        int pilihan = myInput.nextInt();
         switch (pilihan) {
             case 1:
                 System.out.print("Nama hewan peliharaan : ");
-                String NK1 = Input.next();
+                String NK1 = myInput.next();
                 System.out.print("Ras : ");
-                String RK1 = Input.next();
+                String RK1 = myInput.next();
                 System.out.print("Warna Bulu : ");
-                String WK1 = Input.next();
+                String WK1 = myInput.next();
                 Kucing K1 = new Kucing(RK1, NK1, WK1);
                 K1.displayDetailKucing();
                 break;
             case 2:
                 System.out.print("Nama hewan peliharaan : ");
-                String NA1 = Input.next();
+                String NA1 = myInput.next();
                 System.out.print("Ras : ");
-                String RA1 = Input.next();
+                String RA1 = myInput.next();
                 System.out.print("Warna Bulu : ");
-                String WA1 = Input.next();
+                String WA1 = myInput.next();
                 System.out.print("Kemampuan : ");
-                Input.nextLine();
-                String[] kemampuan = Input.nextLine().split(", ");
+                myInput.nextLine();
+                String[] kemampuan = myInput.nextLine().split(", ");
                 Anjing A1 = new Anjing(RA1, NA1, WA1, kemampuan);
                 A1.displayDetailAnjing();
                 break;
